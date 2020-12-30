7c30fc248aa5d663ea2f52fb79d2740dcc2afbc8
Gustavo Augusto <gacl@ic.ufal.br>
2020-12-30 14:38:16 -0300

    class to use in case of errors generated from app

---
+++ src/errors/AppError.ts
@@ -0,0 +1,11 @@
+class AppError {
+  statusCode: number;
+  message: string;
+
+  constructor(message: string, statusCode = 400) {
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
+
+export default AppError;
