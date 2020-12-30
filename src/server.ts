2b2832ea6af7ff5812deddbbaf10e4718a825700
Gustavo Augusto <gacl@ic.ufal.br>
2020-12-30 14:40:39 -0300

    Add middleware to use in cases of errors generated in app

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,37 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Response, Request, NextFunction } from 'express';
+import 'express-async-errors';
+import routes from './routes';
+
+import AppError from './errors/AppError';
+
+// Injecoes de dependencias
+import './container';
+// Conexao com DB
+import './database';
 
 const app = express();
 
-app.listen(3333, () => {
-  console.log("Executando...")
-});
+app.use(express.json());
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000');
+})
