8ae4b0b44db1320e64aa3e601c3701d03121cacc
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-02 18:41:02 -0300

    Create Secretariat controller with index and create functions

---
+++ src/controllers/SecretariatsController.ts
@@ -0,0 +1,29 @@
+import {Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import SecretariatsService from '../services/SecretariatsService';
+
+class SecretariatsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { type, departament_id } = request.body;
+
+    const createSecretariat = container.resolve(SecretariatsService);
+
+    const secretariat = await createSecretariat.createSecretariat({
+      type,
+      departament_id
+    });
+
+    return response.json(secretariat);
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listAllSecretariats = container.resolve(SecretariatsService);
+
+    const secretariats = await listAllSecretariats.listAllSecretariats();
+
+    return response.json(secretariats);
+  }
+}
+
+export default SecretariatsController;
