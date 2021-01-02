1beabe11dd8a48cf5f7985bdb09ecde84b45dff0
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-02 19:44:55 -0300

    Create teacher repository

---
+++ src/repositories/TeacherRepository.ts
@@ -0,0 +1,62 @@
+import { getRepository, Repository } from 'typeorm';
+
+import ICreateTeacherDTO from '../dtos/ICreateTeacherDTO';
+import ITeacherRepository from './ITeacherRepository';
+
+import Teacher from '../models/Teacher';
+
+class TeacherRepository implements ITeacherRepository {
+  private ormRepository: Repository<Teacher>;
+
+  constructor() {
+    this.ormRepository = getRepository(Teacher);
+  }
+
+  public async create(data: ICreateTeacherDTO): Promise<Teacher> {
+    const teacher = this.ormRepository.create(data);
+
+    await this.ormRepository.save(teacher);
+
+    return teacher;
+  }
+
+  public async findByCpf(cpf: string): Promise<Teacher | undefined> {
+    const teacher = await this.ormRepository.findOne({
+      where: {
+        cpf,
+      }
+    });
+
+    return teacher;
+  }
+
+  public async findByEmail(email: string): Promise<Teacher | undefined> {
+    const teacher = await this.ormRepository.findOne({
+      where: {
+        email,
+      }
+    });
+
+    return teacher;
+  }
+
+  public async findAll(): Promise<Teacher[]> {
+    const teacher = await this.ormRepository.find({
+      relations: ['departament']
+    });
+
+    return teacher;
+  }
+
+  // public async findById(id: string): Promise<Departament | undefined> {
+  //   const departament = await this.ormRepository.findOne({
+  //     where: {
+  //       id,
+  //     }
+  //   });
+
+  //   return departament;
+  // }
+}
+
+export default TeacherRepository;
