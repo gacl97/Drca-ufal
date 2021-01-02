640669c94e8661ef582d21dd0c3764630ae27b0d
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-02 18:37:35 -0300

    Update to inject Departament and Secreatariat repositories

--- src/container/index.ts
+++ src/container/index.ts
@@ -1,1 +1,18 @@
 import { container } from 'tsyringe';
+
+import IDepartamentRepository from '../repositories/IDepartamentRepository';
+import DepartamentRepository from '../repositories/DepartamentRepository';
+
+import ISecretariatRepository from '../repositories/ISecretariatRepository';
+import SecretariatRepository from '../repositories/SecretariatRepository';
+
+
+container.registerSingleton<IDepartamentRepository>(
+  'DepartamentRepository',
+  DepartamentRepository
+);
+
+container.registerSingleton<ISecretariatRepository>(
+  'SecretariatRepository',
+  SecretariatRepository
+);
