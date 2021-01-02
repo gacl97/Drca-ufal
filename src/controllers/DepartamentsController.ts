aa66c9cc16f7007cefe1d458a3b099b50e6a7161
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-02 18:39:05 -0300

    Create Departament controller with index and create functions

---
+++ src/controllers/DepartamentsController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import DepartamentsService from '../services/DepartamentsService';
+
+class DepartamentsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name } = request.body;
+
+    const createDepartament = container.resolve(DepartamentsService);
+
+    const departament = await createDepartament.createDepartament({
+      name
+    });
+
+    return response.json(departament);
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listAllDepartaments = container.resolve(DepartamentsService);
+
+    const departaments = await listAllDepartaments.listAllDepartaments();
+
+    return response.json(departaments);
+  }
+}
+
+export default DepartamentsController;
