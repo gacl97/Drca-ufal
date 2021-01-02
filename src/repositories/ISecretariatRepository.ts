0822cea7f9c753420ced970309ecf3b40b67994c
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-02 18:44:54 -0300

    Interface of secretariats

---
+++ src/repositories/ISecretariatRepository.ts
@@ -0,0 +1,10 @@
+import Secretariat from "../models/Secretariat";
+
+import SecretariatType from '../models/enums/SecretariatType';
+import ICreateSecretariatDTO from "../dtos/ICreateSecretariatDTO";
+
+export default interface ISecretariatRepository {
+  create(data: ICreateSecretariatDTO): Promise<Secretariat>;
+  findByType(type: SecretariatType): Promise<Secretariat | undefined>;
+  findAll(): Promise<Secretariat[]>
+}
