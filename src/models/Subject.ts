9cd7142f24ae6aa5f9f96097f6aaca888a996e45
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-02 18:41:39 -0300

    Model of subjects

---
+++ src/models/Subject.ts
@@ -0,0 +1,54 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
+
+import SubjectType from './enums/SubjectType';
+import Secretariat from './Secretariat';
+import StudentToSubject from './StudentToSubject';
+
+import Teacher from './Teacher';
+
+@Entity('subjects')
+class Subject {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Teacher, teacher => teacher.id)
+  @JoinColumn({ name: 'teacher_id' })
+  teacher: Teacher;
+
+  @ManyToOne(() => Secretariat, secretariat => secretariat.id)
+  @JoinColumn({ name: 'secretariat_id' })
+  secretariat: Secretariat;
+
+  @Column()
+  secretariat_id: string;
+
+  @Column()
+  teacher_id: string;
+  
+  @OneToMany(() => StudentToSubject, studentToSubject => studentToSubject.subject)
+  studentToSubject: StudentToSubject[];
+
+  @Column()
+  name: string;
+
+  @Column()
+  code: string;
+
+  @Column('int')
+  credits: number;
+
+  @Column({ unique: true })
+  minimum_credits: string;
+
+  @Column('int')
+  subject_type: SubjectType;
+ 
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Subject;
