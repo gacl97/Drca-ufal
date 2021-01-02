d2a316348c2343bcdd23f6a2718dc5bc1d75b293
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-02 19:45:18 -0300

    Add teachers in routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import departamentsRoutes from './departaments.routes';
 import secretariatsRoutes from './secretariats.routes';
+import teachersRoutes from './teachers.routes';
 
 const routes = Router();
 
 routes.use('/departaments', departamentsRoutes);
 routes.use('/secretariats', secretariatsRoutes);
+routes.use('/teachers', teachersRoutes);
 
 export default routes;
