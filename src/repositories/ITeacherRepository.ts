f2e2ce32e9aba0e123fb87a535e0979f458a772d
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-02 19:44:48 -0300

    Create teacher inteface for repository

---
+++ src/repositories/ITeacherRepository.ts
@@ -0,0 +1,9 @@
+import ICreateTeacherDTO from "../dtos/ICreateTeacherDTO";
+import Teacher from "../models/Teacher";
+
+export default interface ITeacherRepository {
+  create(data: ICreateTeacherDTO): Promise<Teacher>;
+  findByCpf(cpf: string): Promise<Teacher | undefined>;
+  findByEmail(email: string): Promise<Teacher | undefined>;
+  findAll(): Promise<Teacher[]>;
+}
