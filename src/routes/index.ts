8cf3e2d81dde8da7b5234cf07ba77226c98ca4c3
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-02 18:42:25 -0300

    File to group all routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import departamentsRoutes from './departaments.routes';
+import secretariatsRoutes from './secretariats.routes';
+
 const routes = Router();
+
+routes.use('/departaments', departamentsRoutes);
+routes.use('/secretariats', secretariatsRoutes);
 
 export default routes;
