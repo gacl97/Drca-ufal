aa3f35898e680d8e4a885c605779fc6dee96ae69
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-03 19:06:59 -0300

    Add subject routes to main routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import departamentsRoutes from './departaments.routes';
 import secretariatsRoutes from './secretariats.routes';
 import teachersRoutes from './teachers.routes';
 import studentsRoutes from './students.routes';
+import subjectsRoutes from './subjects.routes';
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use('/departaments', departamentsRoutes);
 routes.use('/secretariats', secretariatsRoutes);
 routes.use('/teachers', teachersRoutes);
 routes.use('/students', studentsRoutes);
+routes.use('/subjects', subjectsRoutes);
 
 export default routes;
