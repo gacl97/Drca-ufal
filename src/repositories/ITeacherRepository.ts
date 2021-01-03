2327fc7dfe5952873a657ad230cb4f8c3c84488c
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-03 19:06:07 -0300

    Add method findById to find a teacher by id

--- src/repositories/ITeacherRepository.ts
+++ src/repositories/ITeacherRepository.ts
@@ -5,5 +5,6 @@ export default interface ITeacherRepository {
   create(data: ICreateTeacherDTO): Promise<Teacher>;
   findByCpf(cpf: string): Promise<Teacher | undefined>;
   findByEmail(email: string): Promise<Teacher | undefined>;
+  findById(id: string): Promise<Teacher | undefined>;
   findAll(): Promise<Teacher[]>;
 }
