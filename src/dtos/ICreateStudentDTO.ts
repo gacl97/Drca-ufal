0aa5518db9a0d0ce16efa41ebde0f79e2eef28fb
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-03 17:57:13 -0300

    DTO of student

---
+++ src/dtos/ICreateStudentDTO.ts
@@ -0,0 +1,11 @@
+import StudyShiftType from "../models/enums/StudyShiftType";
+
+export default interface ICreateStudentDTO {
+  name: string;
+  email: string;
+  cpf: string;
+  matriculation: string;
+  departament_id: string;
+  study_shift: StudyShiftType;
+  current_credits: number;
+}
