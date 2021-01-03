746975247a673f21543fdd0075da2e1cc6b787b7
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-03 19:04:40 -0300

    Create subjects controller to list and create a subject

---
+++ src/controllers/SubjectsController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import SubjectsService from '../services/SubjectsService';
+
+class SubjectsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { 
+    name,
+    code,
+    credits,
+    minimum_credits,
+    subject_type,
+    teacher_id,
+    secretariat_id,
+  } = request.body;
+
+    const createSubject = container.resolve(SubjectsService);
+
+    const subject = await createSubject.createSubject({
+      name,
+      code,
+      credits,
+      minimum_credits,
+      subject_type,
+      teacher_id,
+      secretariat_id,
+    });
+
+    return response.json(subject);
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listAllSubjects = container.resolve(SubjectsService);
+
+    const subjects = await listAllSubjects.listAllSubjects();
+
+    return response.json(subjects);
+  }
+}
+
+export default SubjectsController;
