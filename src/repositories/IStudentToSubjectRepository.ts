e74ae497164017c2108201da3666eaae4bbe4e93
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-04 08:10:05 -0300

    Create interface to repository to create matriculations

---
+++ src/repositories/IStudentToSubjectRepository.ts
@@ -0,0 +1,7 @@
+import ICreateStudentToSubjectDTO from "../dtos/ICreateStudentToSubjectDTO";
+import StudentToSubject from "../models/StudentToSubject";
+
+export default interface IStudentToSubjectRepository {
+  create(data: ICreateStudentToSubjectDTO): Promise<StudentToSubject>;
+  findAllByStudent(student_id: string): Promise<StudentToSubject[]>;
+}
