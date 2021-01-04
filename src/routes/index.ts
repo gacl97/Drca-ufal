a3d12386226cb34071f94078e161cd11c02c4a95
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-04 08:11:48 -0300

    Add matriculation routes to main routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import secretariatsRoutes from './secretariats.routes';
 import teachersRoutes from './teachers.routes';
 import studentsRoutes from './students.routes';
 import subjectsRoutes from './subjects.routes';
+import matriculationsRoutes from './matriculations.routes';
 
 const routes = Router();
 
@@ -13,5 +14,6 @@ routes.use('/secretariats', secretariatsRoutes);
 routes.use('/teachers', teachersRoutes);
 routes.use('/students', studentsRoutes);
 routes.use('/subjects', subjectsRoutes);
+routes.use('/matriculation', matriculationsRoutes);
 
 export default routes;
