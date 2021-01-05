93cdc17af6fc708e4115cb7b227404fb1671541c
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-05 08:30:14 -0300

    Create two new methods to find by id and by secretariat

--- src/repositories/ISubjectRepository.ts
+++ src/repositories/ISubjectRepository.ts
@@ -4,5 +4,7 @@ import Subject from "../models/Subject";
 export default interface ISubjectRepository {
   create(data: ICreateSubjectDTO): Promise<Subject>;
   findByCode(code: string): Promise<Subject | undefined>;
+  findById(id: string): Promise<Subject | undefined>;
   findAll(): Promise<Subject[]>;
+  findAllBySecretariat(secretariat_id: string): Promise<Subject[]>;
 }
