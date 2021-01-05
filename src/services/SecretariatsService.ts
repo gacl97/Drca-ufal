02488dbcd1a95d8246d3ad3083fe481888ef0bdc
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-05 08:32:49 -0300

    Updated methos to list all secretariats

--- src/services/SecretariatsService.ts
+++ src/services/SecretariatsService.ts
@@ -2,13 +2,39 @@ import { inject, injectable } from 'tsyringe';
 import AppError from '../errors/AppError';
 
 import SecretariatType from '../models/enums/SecretariatType';
+import SubjectType from '../models/enums/SubjectType';
 import Secretariat from '../models/Secretariat';
+
 import IDeparmentRepository from '../repositories/IDepartamentRepository';
 import ISecretariatRepository from '../repositories/ISecretariatRepository';
+import ISubjectRepository from '../repositories/ISubjectRepository';
+import formatSecretariat from '../utils/formatSecretariat';
+import formatSubject from '../utils/formatSubject';
 
 interface IRequestDTO {
   type: SecretariatType;
   departament_id: string;
+}
+
+interface IResponseListAllSecretariatsDTO {
+  secretariats: {
+    secretariat: {
+      id: string;
+      type: SecretariatType;
+      departament: {
+        id: string;
+        name: string;
+      }
+    };
+    subjects: {
+      id: string;
+      name: string;
+      code: string;
+      credits: number;
+      minimum_credits: number;
+      subject_type: SubjectType;
+    }[];
+  }[]
 }
 
 @injectable()
@@ -18,7 +44,9 @@ class SecretariatsService {
     @inject('SecretariatRepository')
     private secretariatRepository: ISecretariatRepository,
     @inject('DepartamentRepository')
-    private departamentRepository: IDeparmentRepository
+    private departamentRepository: IDeparmentRepository,
+    @inject('SubjectRepository')
+    private subjectRepository: ISubjectRepository
   ) {}
 
   public async createSecretariat({ type, departament_id }: IRequestDTO): Promise<Secretariat> {
@@ -52,17 +80,45 @@ class SecretariatsService {
     return secretariat;
   }
 
-  public async listAllSecretariats(): Promise<Secretariat[]> {
+  public async listAllSecretariats(): Promise<IResponseListAllSecretariatsDTO> {
     const secretariats = await this.secretariatRepository.findAll();
 
-    const formatedSecretariats = secretariats.map(secretariat => {
-      return Object.assign(secretariat, {
-        ...secretariat,
-        type: SecretariatType[secretariat.type]
-      })
-    });
+    const secretariatsToReturn = await Promise.all(secretariats.map(async secretariat => {
+      const subjectsBySecretariat = await this.subjectRepository.findAllBySecretariat(secretariat.id);
 
-    return formatedSecretariats;
+      const formattedSecretariat = formatSecretariat(secretariat);
+
+      const secretariatFormatted = {
+        id: formattedSecretariat.id,
+        type: formattedSecretariat.type,
+        departament: {
+          id: formattedSecretariat.departament.id,
+          name: formattedSecretariat.departament.name
+        }
+      }
+
+      const subjects = subjectsBySecretariat.map(subject => {
+        subject = formatSubject(subject);
+
+        return {
+          id: subject.id,
+          name: subject.name,
+          code: subject.code,
+          credits: subject.credits,
+          minimum_credits: subject.minimum_credits,
+          subject_type: subject.subject_type, 
+        }
+      });
+
+      return {
+        secretariat: secretariatFormatted,
+        subjects
+      };
+    }));
+
+    return {
+      secretariats: secretariatsToReturn
+    };
   }
 }
 
