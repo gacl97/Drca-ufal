51298e4a53d5000910c61450285058ed0b65af1e
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-05 08:28:48 -0300

    Create a new method to find subjects by id

--- src/repositories/IStudentToSubjectRepository.ts
+++ src/repositories/IStudentToSubjectRepository.ts
@@ -4,4 +4,5 @@ import StudentToSubject from "../models/StudentToSubject";
 export default interface IStudentToSubjectRepository {
   create(data: ICreateStudentToSubjectDTO): Promise<StudentToSubject>;
   findAllByStudent(student_id: string): Promise<StudentToSubject[]>;
+  findAllBySubject(subject_id: string): Promise<StudentToSubject[]>;
 }
