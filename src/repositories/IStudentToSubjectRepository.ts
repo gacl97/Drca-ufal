8755ef13ace3c2584b2dd75b2eac9f0db7051216
Gustavo Augusto <gacl@ic.ufal.br>
2021-01-09 20:15:39 -0300

    Create method to find a subject info by student and subject

--- src/repositories/IStudentToSubjectRepository.ts
+++ src/repositories/IStudentToSubjectRepository.ts
@@ -5,4 +5,5 @@ export default interface IStudentToSubjectRepository {
   create(data: ICreateStudentToSubjectDTO): Promise<StudentToSubject>;
   findAllByStudent(student_id: string): Promise<StudentToSubject[]>;
   findAllBySubject(subject_id: string): Promise<StudentToSubject[]>;
+  findOneByStudentAndSubject(student_id: string, subject_id: string): Promise<StudentToSubject | undefined>;
 }
